001599170a86e16f53f319af48aeb3cfb963c930
Yair <yair543210@gmail.com>
2024-02-17 21:43:14 +0200

    (auth) - saving last logged at time on login

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,12 +3,17 @@ import { UserService } from '../user/user.service';
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import { SignInResponseDto } from './dto/sign-in-response.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UserService,
     private readonly jwtService: JwtService,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
   ) {}
 
   async signIn(email: string, pass: string): Promise<SignInResponseDto> {
@@ -21,13 +26,14 @@ export class AuthService {
       throw new UnauthorizedException();
     }
 
+    user.lastLoggedAt = new Date();
+    this.userRepository.save(user);
     // destructuring the user object to remove the password property
     // const { password, ...result } = user;
     // TODO: Generate a JWT and return it here
     // instead of the user object
     const payload = { sub: user.id, email: user.email };
 
-    //TODO: update the last login date
     return {
       token: await this.jwtService.signAsync(payload),
       user: user,
