38080744ee9d227a0512deeab83f5becf32f2edf
yairmishnayot <yair.mishnayot@thrivedx.com>
2024-03-01 12:02:10 +0200

    Get interest in a specific client

--- src/modules/client-interest/client-interest.module.ts
+++ src/modules/client-interest/client-interest.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ClientInterestService } from './client-interest.service';
 import { ClientInterestController } from './client-interest.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientInterest } from './entities/client-interest.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ClientInterest])],
   controllers: [ClientInterestController],
   providers: [ClientInterestService],
+  exports: [TypeOrmModule],
 })
 export class ClientInterestModule {}
--- src/modules/client-interest/client-interest.service.ts
+++ src/modules/client-interest/client-interest.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateClientInterestDto } from './dto/create-client-interest.dto';
 import { UpdateClientInterestDto } from './dto/update-client-interest.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ClientInterest } from './entities/client-interest.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ClientInterestService {
+  constructor(
+    @InjectRepository(ClientInterest)
+    private clientInterestRepository: Repository<ClientInterest>,
+  ) {}
+
   create(createClientInterestDto: CreateClientInterestDto) {
     return 'This action adds a new clientInterest';
   }
@@ -12,8 +20,10 @@ export class ClientInterestService {
     return `This action returns all clientInterest`;
   }
 
-  findAllForClient(clientId: number){
-    return `This action returns all clientInterest for client ${clientId}`;
+  findAllForClient(clientId: number) {
+    return this.clientInterestRepository.find({
+      where: { intrestedInClient: { id: clientId } },
+    });
   }
 
   findOne(id: number) {
