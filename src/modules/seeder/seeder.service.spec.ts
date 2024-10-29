2f6b373d0a3207e453cf67eea22acef2f210736a
yairmishnayot <yair.mishnayot@thrivedx.com>
2024-10-29 21:12:28 +0200

    Fixed more unit tests

--- src/modules/seeder/seeder.service.spec.ts
+++ src/modules/seeder/seeder.service.spec.ts
@@ -1,12 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SeederService } from './seeder.service';
+import { CitySeederService } from './city-seeder/city-seeder.service';
+import { RoleSeederService } from './role-seeder/role-seeder.service';
+import { ProjectSeederService } from './projects-seeder/project-seeder/project-seeder.service';
+import { GroupSeederService } from './group-seeder/group-seeder/group-seeder.service';
+import { UserSeederService } from './user-seeder/user-seeder.service';
+import { EthnicitiesSeederService } from './ethnicities-seeder/ethnicities-seeder/ethnicities-seeder.service';
+import { ReligionsStyleSeederService } from './religions-style-seeder/religions-style-seeder/religions-style-seeder.service';
+import { ClientSeederService } from './client-seeder/client-seeder/client-seeder.service';
 
 describe('SeederService', () => {
   let service: SeederService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SeederService],
+      providers: [
+        SeederService,
+        {
+          provide: CitySeederService,
+          useValue: {
+            // Mock methods if necessary
+          },
+        },
+        {
+          provide: RoleSeederService,
+          useValue: {},
+        },
+        {
+          provide: ProjectSeederService,
+          useValue: {},
+        },
+        {
+          provide: GroupSeederService,
+          useValue: {},
+        },
+        {
+          provide: UserSeederService,
+          useValue: {},
+        },
+        {
+          provide: EthnicitiesSeederService,
+          useValue: {},
+        },
+        {
+          provide: ReligionsStyleSeederService,
+          useValue: {},
+        },
+        {
+          provide: ClientSeederService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<SeederService>(SeederService);
--- src/modules/seeder/user-seeder/user-seeder.service.spec.ts
+++ src/modules/seeder/user-seeder/user-seeder.service.spec.ts
@@ -1,12 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserSeederService } from './user-seeder.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../../user/entities/user.entity'; // Adjust paths if needed
+import { Group } from '../../group/entities/group.entity';
+import { UserGroup } from '../../user-group/entities/user-group.entity';
+import { Role } from '../../role/entities/role.entity';
 
 describe('UserSeederService', () => {
   let service: UserSeederService;
 
+  // Mock repository with basic functions
+  const mockRepository = {
+    find: jest.fn(),
+    save: jest.fn(),
+    create: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserSeederService],
+      providers: [
+        UserSeederService,
+        // Mock UserRepository
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        // Mock GroupRepository
+        {
+          provide: getRepositoryToken(Group),
+          useValue: mockRepository,
+        },
+        // Mock UserGroupRepository
+        {
+          provide: getRepositoryToken(UserGroup),
+          useValue: mockRepository,
+        },
+        // Mock RoleRepository
+        {
+          provide: getRepositoryToken(Role),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserSeederService>(UserSeederService);
