482170a88eb27b86b319dd6647f53ca8bf3d8187
yairmishnayot <yair.mishnayot@thrivedx.com>
2024-10-29 21:17:38 +0200

    client-seeder.service test fix

--- src/modules/seeder/client-seeder/client-seeder/client-seeder.service.spec.ts
+++ src/modules/seeder/client-seeder/client-seeder/client-seeder.service.spec.ts
@@ -1,12 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientSeederService } from './client-seeder.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Client } from '@/modules/client/entities/client.entity';
+import { User } from '@/modules/user/entities/user.entity';
+import { UserGroup } from '@/modules/user-group/entities/user-group.entity';
+import { City } from '@/modules/city/entities/city.entity';
+import { ReligionStyle } from '@/modules/religion-style/entities/religion-style.entity';
+import { Ethnicity } from '@/modules/ethnicity/entities/ethnicity.entity';
 
 describe('ClientSeederService', () => {
   let service: ClientSeederService;
 
+  // Mock repository with basic functions
+  const mockRepository = {
+    find: jest.fn(),
+    save: jest.fn(),
+    create: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ClientSeederService],
+      providers: [
+        ClientSeederService,
+        // Mock ClientRepository
+        {
+          provide: getRepositoryToken(Client),
+          useValue: mockRepository,
+        },
+        // Mock UserRepository
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        // Mock UserGroupRepository
+        {
+          provide: getRepositoryToken(UserGroup),
+          useValue: mockRepository,
+        },
+        // Mock CityRepository
+        {
+          provide: getRepositoryToken(City),
+          useValue: mockRepository,
+        },
+        // Mock ReligionStyleRepository
+        {
+          provide: getRepositoryToken(ReligionStyle),
+          useValue: mockRepository,
+        },
+        // Mock EthnicityRepository
+        {
+          provide: getRepositoryToken(Ethnicity),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ClientSeederService>(ClientSeederService);
