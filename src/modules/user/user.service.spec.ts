fc7b6a7945d2225897b291e1ef8fc607e7326659
yairmishnayot <yair.mishnayot@thrivedx.com>
2024-11-07 07:57:24 +0200

    Fixed user service tests

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,15 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UserGroup } from '../user-group/entities/user-group.entity';
+import { Repository } from 'typeorm';
 
 describe('UserService', () => {
   let service: UserService;
+  let userRepository: Repository<User>;
+  let userGroupRepository: Repository<UserGroup>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository, // Mock repository for User
+        },
+        {
+          provide: getRepositoryToken(UserGroup),
+          useClass: Repository, // Mock repository for UserGroup
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
+    userGroupRepository = module.get<Repository<UserGroup>>(
+      getRepositoryToken(UserGroup),
+    );
   });
 
   it('should be defined', () => {
